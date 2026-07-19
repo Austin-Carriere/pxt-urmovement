b8856bdcedeca4ba9691fe297db476459c4fd790
Austin-Carriere <austin.carriere@jesuitnola.org>
2026-07-19 08:41:25 -0500

    Vs Code stuff

--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
-input.onButtonPressed(Button.A, function () {
-    RingbitCar.steering_angle(RingbitCar.Direction_turn.left, 600)
-    basic.pause(2000)
-    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 600)
-})
-input.onButtonPressed(Button.AB, function () {
-    RingbitCar.freestyle(100, 100)
-    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 900)
-    RingbitCar.turnright()
-})
-input.onButtonPressed(Button.B, function () {
-    RingbitCar.brake()
-})
-basic.forever(function () {
-    basic.showNumber(Brickcell.getGreen())
-})
+/**
+ * Drive a robot using continuous rotation servos.
+ */
+//% color="#f4e008" icon="\uf1b9" block="Movement"
+namespace Movement {
+
+}
+
+/**
+ * Drive forward for a number of seconds.
+ */
+//% block="forward for %seconds seconds"
+//% seconds.min=0
+//% seconds.defl=0
+//% weight=100
+export function forward(seconds: number): void {
+
+    // Left wheel forward
+    pins.servoWritePin(AnalogPin.P1, 180)
+
+    // Right wheel forward (reversed because it's mounted backwards)
+    pins.servoWritePin(AnalogPin.P2, 0)
+
+    basic.pause(seconds * 1000)
+
+    // Stop
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
+}
